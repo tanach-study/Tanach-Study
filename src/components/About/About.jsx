42ed3132ad3b34c40f09a78bc265ec6a40e31385
Joey Pinhas <jpinhas97@yahoo.com>
2016-12-26 17:42:25 -0500

    About page layout boilerplate

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,51 @@ class About extends Component {
   render() {
     return (
       <div className="container">
-        About
+        <div className="row valign-wrapper">
+          <div className="col l9">
+            <div className="col-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iusto, impedit mollitia cum libero atque quasi itaque, voluptatem in maxime alias quibusdam laborum voluptas nesciunt nisi labore consectetur similique aliquam.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+            </div>
+          </div>
+          <div className="col l3">
+            <div className="col-content">
+              <img src="/assets/images/background1.jpg" alt="" className="valign responsive-img"/>
+            </div>
+          </div>
+        </div>
+        <div className="row valign-wrapper">
+          <div className="col l3">
+            <div className="col-content">
+              <img src="/assets/images/background2.jpg" alt="" className="valign responsive-img"/>
+            </div>
+          </div>
+          <div className="col l9">
+            <div className="col-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iusto, impedit mollitia cum libero atque quasi itaque, voluptatem in maxime alias quibusdam laborum voluptas nesciunt nisi labore consectetur similique aliquam.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+            </div>
+          </div>
+        </div>
+        <div className="row valign-wrapper">
+          <div className="col l9">
+            <div className="col-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores iusto, impedit mollitia cum libero atque quasi itaque, voluptatem in maxime alias quibusdam laborum voluptas nesciunt nisi labore consectetur similique aliquam.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita exercitationem neque ea consequatur, maiores dolore placeat reiciendis, ut, quaerat accusamus qui porro minus ullam quod possimus amet assumenda culpa excepturi.
+            </div>
+          </div>
+          <div className="col l3">
+            <div className="col-content">
+              <img src="/assets/images/background3.jpg" alt="" className="valign responsive-img"/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
