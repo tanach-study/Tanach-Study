969456012e27ec02c8acd2495f4c07355a0a33af
Joey Pinhas <jpinhas97@yahoo.com>
2017-01-31 11:17:05 -0500

    Adds those two new components

---
+++ src/components/Signup/SignupError/SignupError.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SignupError = props => {
+  return (
+    <div>
+      YOU ARE A FAILURE
+    </div>
+  )
+}
+
+export default SignupError;
---
+++ src/components/Signup/SignupSuccess/SignupSuccess.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const SignupSuccess = props => {
+  return (
+    <div>
+      This is what success looks like...
+    </div>
+  )
+}
+
+export default SignupSuccess;
