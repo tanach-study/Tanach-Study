91b09a60999617e4bab4a69e197054cf00662cec
Joey Pinhas <jpinhas97@yahoo.com>
2017-02-01 09:47:16 -0500

    Adds a list of all teachers that link to their individual pages

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Teachers extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      teachers: [],
+    }
+  }
+
+  updateState(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+
+  componentWillMount() {
+    window.scrollTo(0, 0);
+  }
+
+  componentDidMount() {
+    fetch('/api/teachers')
+    .then(r => r.json())
+    .then(data => this.updateState('teachers', data))
+    .catch(err => console.log(err));
   }
 
   render() {
+    const mappedTeachers = this.state.teachers.map((teacher, i) => {
+      return (
+        <div className="col l4 m6 s12" key={i}>
+          <Link to={`/teachers/${teacher.teacher_id}`} className="right-align">
+            <div className="card small hoverable full-width black-text">
+              <div className="card-image">
+                <img src="/assets/images/background2.jpg" alt=""/>
+              </div>
+              <div className="card-content">
+                <div className="card-title left-align">{teacher.title} {teacher.fname}{teacher.mname ? ` ${teacher.mname} ` : ' '}{teacher.lname}</div>
+                <p className="left-align"><i>{teacher.short_bio}</i></p>
+              </div>
+            </div>
+          </Link>
+        </div>
+      );
+    });
     return (
-      <div className="">
-        List of teachers
+      <div className="container">
+        <div className="section center">
+          <div className="heading">
+            <h2>Our Teachers</h2>
+            <p>Below is a list of all of the educators that have taught daily perakim in our program.</p>
+          </div>
+          <div className="teachers">
+            <div className="row">
+              {mappedTeachers}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
