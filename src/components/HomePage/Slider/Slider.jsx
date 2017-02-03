881bb07e47cb4a9aac41ff99b626767885b949ce
Joey Pinhas <jpinhas97@yahoo.com>
2017-02-03 12:42:17 -0500

    Updates slider to work purely in react/css

--- src/components/HomePage/Slider/Slider.jsx
+++ src/components/HomePage/Slider/Slider.jsx
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 class Slider extends Component {
   constructor(props) {
     super(props);
-    const slides = this.props.slides.map((slide, i) => {
-      return (
-        <li key={i}>
-          <img src={slide.url} alt=""/>
-          <div className="caption left-align">
-            <h5>{slide.captionLeft}</h5>
-          </div>
-          <div className="caption right-align">
-            <h5>{slide.captionRight}</h5>
-          </div>
-        </li>
-      )
-    });
     this.state = {
       activeIndex: 0,
-      allSlides: slides || [],
-      bgImage: '',
     }
   }
 
@@ -29,22 +15,47 @@ class Slider extends Component {
     });
   }
 
+  componentDidMount() {
+    const interval = setInterval(() => {
+      this.advanceSlide();
+    }, 50000)
+  }
+
+  advanceSlide() {
+    const curr = this.state.activeIndex;
+    const numSlides = this.props.slides.length;
+    let next = curr + 1;
+    if (next >= numSlides) next = 0;
+    this.updateState('activeIndex', next);
+  }
+
   render() {
+    const slides = this.props.slides.map((slide, i) => {
+      const style = {
+        backgroundImage: `url(${slide.url})`,
+      }
+      const slideClass = classNames({
+        'slide-item': true,
+        active: this.state.activeIndex == i,
+        inactive: this.state.activeIndex != i,
+      });
+      return (
+        <div key={i} className={slideClass} style={style}>
+          <div className="caption left-align">
+            <h5>{slide.captionLeft}</h5>
+          </div>
+          <div className="caption right-align">
+            <h5>{slide.captionRight}</h5>
+          </div>
+        </div>
+      )
+    });
     return (
-      <ul className="slides">
-        {this.state.allSlides[this.state.activeIndex]}
-      </ul>
+      <div className="slideshow">
+        {slides}
+      </div>
     );
   }
 }
-
-/*
-  const imageComps = props.images.map((image, i) => <img src={image.url} alt=""/>)
-        <img src={props.images[props.index].url} alt=""/>
- <div className="image-container">
-        {imageComps[props.index]}
-        }
-      </div>
-      */
 
 export default Slider;
