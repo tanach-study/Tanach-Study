39cea3ea206c91d2999ec8a0e5dc42c7595fb537
Joey Pinhas <jpinhas97@yahoo.com>
2017-02-09 12:01:30 -0500

    Adds our custom auth header to the allowed headers list

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(bodyParser.json());
 // allow CORS on the entire site
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token_authorization");
   next();
 });
 
