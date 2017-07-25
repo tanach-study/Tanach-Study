8b55061efdce08fb476926ace103021bfbdafac9
Joey Pinhas <jpinhas97@yahoo.com>
2017-07-24 23:01:03 -0400

    Updates all of the dev dependencies to the latest verisons, and updates the webpack config to reflect these changes

---
+++ webpack.config.old.js
@@ -0,0 +1,79 @@
+'use strict'
+const webpack           = require('webpack');
+const path              = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const BUILD_DIR         = path.resolve(__dirname, 'dist');
+const APP_DIR           = path.resolve(__dirname, 'src');
+
+
+module.exports = {
+  entry: ['whatwg-fetch', `${APP_DIR}/index.js`],
+  output: {
+    path: BUILD_DIR,
+    filename: '/js/[name].js',
+  },
+  cache: true,
+  debug: true,
+  devtool: 'eval-source-map',
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  plugins: [
+    new ExtractTextPlugin('/css/[name].css', {
+      allChunks: true
+    })
+  ],
+
+  module : {
+    include: path.join(__dirname, 'src'),
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      {
+        test: /\.gif$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      {
+        test: /\.png$/,
+        loader: 'file-loader?name=/img/[name].[hash:base64:5].[ext]'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=/fonts/[name].[ext]'
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=100&mimetype=application/font-woff&name=/fonts/[name].[ext]'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=100&mimetype=application/octet-stream&name=/fonts/[name].[ext]'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=/fonts/[name].[ext]'
+      }
+    ]
+  }
+};
