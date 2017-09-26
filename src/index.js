e220dad49d274b42a6e4b66f480306ef0e35d633
Joey Pinhas <jpinhas97@yahoo.com>
2017-09-26 16:21:49 -0400

    Adds Google Analytics package, adds code to send tracking data on each page load, and adds a package lock file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 import 'whatwg-fetch';
 
-// mount a new router with the routes from the other file at #root-container
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-53814701-1');
+
+// https://github.com/ReactTraining/react-router/issues/4278#issuecomment-299692502
+const withTracker = (WrappedComponent) => {
+  const trackPage = (page) => {
+    ReactGA.set({ page });
+    ReactGA.pageview(page);
+  };
+
+  const HOC = (props) => {
+    const page = props.location.pathname + props.location.search;
+    trackPage(page);
+
+    return (
+      <WrappedComponent {...props} />
+    );
+  };
+
+  return HOC;
+};
+
 ReactDOM.render(
   <BrowserRouter>
-    <Route path='/' component={App} />
+    <Route component={withTracker(App)} />
   </BrowserRouter>,
   document.querySelector('#root-container')
 );
