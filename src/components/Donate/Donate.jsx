a2133087c629f207dfcc4a077c6460ea66498820
Joey Pinhas <jpinhas97@yahoo.com>
2017-10-03 15:39:12 -0400

    Tweaks layout of donation page a little

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -20,26 +20,21 @@ class Donate extends Component {
       <div className="container">
         <div className="section">
           <div className="row">
-            <div className="col s12 hide-on-med-and-up card">
-              <div className="col-content">
-                <h4>Become a Contributor</h4>
-                <p>Yes, it’s a free program, but we couldn’t do it without your support.</p>
-                <p>Whatever you can help out with is greatly appreciated.</p>
-                <p>Special thanks to those who enrolled in the monthly sponsorship.</p>
-                <p>For further inquiries – please <Link to="/contact">contact us here.</Link></p>
+            <div className="col l8 m8 s12 push-l2 push-m2">
+              <div className="card">
+                <div className="card-content center">
+                  <h4>Become a Contributor</h4>
+                  <p>Yes, it’s a free program, but we couldn’t do it without your support.</p>
+                  <p>Whatever you can help out with is greatly appreciated.</p>
+                  <p>Special thanks to those who enrolled in the monthly sponsorship.</p>
+                  <p>For further inquiries – please <Link to="/contact">contact us here.</Link></p>
+                  <br/>
+                  <i>Tanach Study is a registered 501(c)(3) organization.</i>
+                </div>
               </div>
             </div>
-            <div className="col l6 m8 s12">
+            <div className="col l8 m8 s12 push-l2 push-m2 center">
               <script src="https://donorbox.org/widget.js" type="text/javascript"></script><iframe src="https://donorbox.org/embed/support-tanach-study" height="1000px" width="100%" style={styling} seamless="seamless" id="dbox-form-embed" name="donorbox" frameBorder="0" scrolling="no"></iframe>
-            </div>
-            <div className="col l6 m4 hide-on-small-only card">
-              <div className="col-content">
-                <h4>Become a Contributor</h4>
-                <p>Yes, it’s a free program, but we couldn’t do it without your support.</p>
-                <p>Whatever you can help out with is greatly appreciated.</p>
-                <p>Special thanks to those who enrolled in the monthly sponsorship.</p>
-                <p>For further inquiries – please <Link to="/contact">contact us here.</Link></p>
-              </div>
             </div>
           </div>
         </div>
