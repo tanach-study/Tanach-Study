158e0745eb4085aded9f39b8166d069639ee9af3
Joey Pinhas <jpinhas97@yahoo.com>
2017-12-29 14:10:56 -0500

    Updates the TorahPerek component to use the proper chapter numbers for each parasha

--- src/components/Perakim/TorahPerek.jsx
+++ src/components/Perakim/TorahPerek.jsx
@@ -163,31 +163,42 @@ class TorahPerek extends Component {
   showSefer() {
     const hebArr = this.props.act.hebrew_text || [];
     const engArr = this.props.act.english_text || [];
+    const start_chapter = this.props.act.start_chapter || null;
+    const start_verse = this.props.act.start_verse || null;
+    const end_chapter = this.props.act.end_chapter || null;
+    const end_verse = this.props.act.end_verse || null;
+
     let gen = Date.now();
 
     const heb = [];
     const eng = [];
     hebArr.forEach((perek, i) => {
-      heb.push(<b key={gen++}>פרק {gematriya(i + 1)}</b>)
+      const perek_num = start_chapter != null ? start_chapter + i : i + 1;
+
+      heb.push(<b key={gen++}>פרק {gematriya(perek_num)}</b>)
       perek.forEach((passuk, j) => heb.push(<p key={gen++}><b>{gematriya(j + 1)}. </b>{passuk}</p>));
     });
     engArr.forEach((perek, i) => {
-      eng.push(<b key={gen++}>Perek {(i + 1)}</b>)
+      const perek_num = start_chapter != null ? start_chapter + i : i + 1;
+
+      eng.push(<b key={gen++}>Perek {perek_num}</b>)
       perek.forEach((passuk, j) => eng.push(<p key={gen++}><b>{i + 1}. </b>{passuk}</p>));
     });
     let par = [];
     for (let i = 0; i < hebArr.length && i < engArr.length; i++) {
+      const perek_num = start_chapter != null ? start_chapter + i : i + 1;
+
       let currHeb = hebArr[i];
       let currEng = engArr[i];
       par.push(
         <div key={gen++}>
           <div className="row valign-wrapper hide-on-small-only">
-            <div className="col l6 m12 s12 left-align valign"><b>Perek {i + 1}</b></div>
-            <div className="col l6 m12 s12 rtl right-align valign"><b>פרק {gematriya(i + 1)}</b></div>
+            <div className="col l6 m12 s12 left-align valign"><b>Perek {perek_num}</b></div>
+            <div className="col l6 m12 s12 rtl right-align valign"><b>פרק {gematriya(perek_num)}</b></div>
           </div>
           <div className="hide-on-med-and-up">
-            <div className="row center-align rtl"><b>פרק {gematriya(i + 1)}</b></div>
-            <div className="row center-align"><b>Perek {i + 1}</b></div>
+            <div className="row center-align rtl"><b>פרק {gematriya(perek_num)}</b></div>
+            <div className="row center-align"><b>Perek {perek_num}</b></div>
           </div>
         </div>
       );
