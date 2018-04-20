d2983ded7a6ea4d37829d37bca950b0033f05f3f
Joey Pinhas <jpinhas97@yahoo.com>
2018-04-20 00:51:00 -0400

    Slims down root yerushalmi component by making it a function

--- src/components/Yerushalmi/Yerushalmi.jsx
+++ src/components/Yerushalmi/Yerushalmi.jsx
@@ -1,46 +1,29 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 import BookList from './BookList/BookList.jsx';
 
 import data from './yerushalmi.json';
 
-class Yerushalmi extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      torah: data.torah || [],
-      neviim: data.neviim || [],
-      ketuvim: data.ketuvim || [],
-    }
-  }
-
-  componentWillMount() {
-    window.scrollTo(0, 0);
-  }
-
-  render() {
-    return (
-      <div className='container'>
-          <div className='section'>
-            <h3 className='center'>Sefarim</h3>
-            <BookList
-              title='Torah'
-              section={this.state.torah}
-            />
-            <BookList
-              title='Neviim'
-              section={this.state.neviim}
-            />
-            <BookList
-              title='Ketuvim'
-              section={this.state.ketuvim}
-            />
-          </div>
+function Yerushalmi (props) {
+  return (
+    <div className='container'>
+        <div className='section'>
+          <h3 className='center'>Sefarim</h3>
+          <BookList
+            title='Torah'
+            section={data.torah || []}
+          />
+          <BookList
+            title='Neviim'
+            section={data.neviim || []}
+          />
+          <BookList
+            title='Ketuvim'
+            section={data.ketuvim || []}
+          />
         </div>
-    );
-  }
-
+      </div>
+  );
 }
 
 export default Yerushalmi;
