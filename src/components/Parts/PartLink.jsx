ba61c4b1c611608e8df9f4d3d695f3d6b3bc2ccd
Joey Pinhas <jpinhas97@yahoo.com>
2018-04-26 00:03:55 -0400

    Cleans up the parts component, and splits it into smaller chunks

---
+++ src/components/Parts/PartLink.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PartLink ({book, i, books}) {
+  let classAdd = '';
+  const diff = books.length - i;
+  if (diff < 3 && diff > 0) {
+    if (books.length % 3 == 1 && i == books.length - 1) {
+      classAdd = i % 2 == 0 ? 'offset-l4 offset-m3' : 'offset-l4';
+    } else if (books.length % 3 == 2 && i == books.length - 2) {
+      classAdd = i % 2 == 0 ? 'offset-l2 offset-m3' : 'offset-l2';
+    }
+  }
+
+  return (
+    <Link to={book.url} className={`col s12 m6 l4 ${classAdd}`} key={i}>
+      <div className='card tsblue btn waves-effect hoverable full-width'>
+        <div className='col-content'>{book.name}</div>
+      </div>
+    </Link>
+  );
+}
+
+export default PartLink;
--- src/components/Parts/Parts.jsx
+++ src/components/Parts/Parts.jsx
@@ -1,54 +1,17 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
+import PartList from './PartList.jsx';
 
 import data from './parts.json';
 
-function bookMapper (book, i, books) {
-  let classAdd = '';
-  const diff = books.length - i;
-  if (diff < 3 && diff > 0) {
-    if (books.length % 3 == 1 && i == books.length - 1) {
-      classAdd = i % 2 == 0 ? 'offset-l4 offset-m3' : 'offset-l4';
-    } else if (books.length % 3 == 2 && i == books.length - 2) {
-      classAdd = i % 2 == 0 ? 'offset-l2 offset-m3' : 'offset-l2';
-    }
-  }
-
-  return (
-    <Link to={book.url} className={`col s12 m6 l4 ${classAdd}`} key={i}>
-      <div className='card tsblue btn waves-effect hoverable full-width'>
-        <div className='col-content'>{book.name}</div>
-      </div>
-    </Link>
-  );
-}
-
 function Parts (props) {
-  const mappedTorah = data.torah.map(bookMapper);
-  const mappedNeviim = data.neviim.map(bookMapper);
-  const mappedKetuvim = data.ketuvim.map(bookMapper);
   return (
     <div className='container'>
       <div className='section center'>
         <h4>Sefarim</h4>
-        <div className='row center'>
-          <div className='center center-align light'>
-            <h5>Torah</h5>
-            {mappedTorah}
-          </div>
-        </div>
-        <div className='row center'>
-          <div className='center center-align light'>
-            <h5>Nevi'im</h5>
-            {mappedNeviim}
-          </div>
-        </div>
-        <div className='row center'>
-          <div className='center center-align light'>
-            <h5>Ketuvim</h5>
-            {mappedKetuvim}
-          </div>
-        </div>
+        <PartList title='Torah' parts={data.torah} />
+        <PartList title={'Nevi\'im'} parts={data.neviim} />
+        <PartList title='Ketuvim' parts={data.ketuvim} />
       </div>
     </div>
   );
