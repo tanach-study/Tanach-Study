d174951e30bfc284ad01b4aef1cd39297e0858f3
Joey Pinhas <jpinhas97@yahoo.com>
2018-04-25 23:30:20 -0400

    Cleans up teacher components

--- src/components/Teachers/AllTeachers.jsx
+++ src/components/Teachers/AllTeachers.jsx
@@ -9,12 +9,6 @@ class Teachers extends Component {
     }
   }
 
-  updateState(key, value) {
-    this.setState({
-      [key]: value,
-    });
-  }
-
   componentWillMount() {
     window.scrollTo(0, 0);
   }
@@ -22,22 +16,22 @@ class Teachers extends Component {
   componentDidMount() {
     fetch('/api/teachers')
     .then(r => r.json())
-    .then(data => this.updateState('teachers', data))
+    .then(data => this.setState({ teachers: data }))
     .catch(err => console.log(err));
   }
 
   render() {
     const mappedTeachers = this.state.teachers.map((teacher, i) => {
       return (
-        <div className="col l4 m6 s12" key={i}>
-          <Link to={`/teachers/${teacher.teacher_info.teacher_id}`} className="right-align">
-            <div className="card medium hoverable full-width black-text">
-              <div className="card-image">
-                <img src={teacher.teacher_info.image_url} alt=""/>
+        <div className='col l4 m6 s12' key={i}>
+          <Link to={`/teachers/${teacher.teacher_info.teacher_id}`} className='right-align'>
+            <div className='card medium hoverable full-width black-text'>
+              <div className='card-image'>
+                <img src={teacher.teacher_info.image_url} alt=''/>
               </div>
-              <div className="card-content">
-                <div className="card-title left-align">{teacher.teacher_info.title} {teacher.teacher_info.fname}{teacher.teacher_info.mname ? ` ${teacher.teacher_info.mname} ` : ' '}{teacher.teacher_info.lname}</div>
-                <p className="left-align"><i>{teacher.teacher_info.short_bio}</i></p>
+              <div className='card-content'>
+                <div className='card-title left-align'>{teacher.teacher_info.title} {teacher.teacher_info.fname}{teacher.teacher_info.mname ? ` ${teacher.teacher_info.mname} ` : ' '}{teacher.teacher_info.lname}</div>
+                <p className='left-align'><i>{teacher.teacher_info.short_bio}</i></p>
               </div>
             </div>
           </Link>
@@ -45,14 +39,14 @@ class Teachers extends Component {
       );
     });
     return (
-      <div className="container">
-        <div className="section center">
-          <div className="heading">
+      <div className='container'>
+        <div className='section center'>
+          <div className='heading'>
             <h2>Our Teachers</h2>
             <p>Below is a list of all of the educators that have taught daily perakim in our program.</p>
           </div>
-          <div className="teachers">
-            <div className="row">
+          <div className='teachers'>
+            <div className='row'>
               {mappedTeachers}
             </div>
           </div>
