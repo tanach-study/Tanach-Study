e1e500298e04f7d0bdcc3ba3d73fe7d42b27a24f
Joey Pinhas <jpinhas97@yahoo.com>
2018-04-25 23:33:16 -0400

    Cleans up sign up components

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -10,35 +10,29 @@ class Signup extends Component {
     }
   }
 
-  updateState(key, value) {
-    this.setState({
-      [key]: value,
-    });
-  }
-
   componentWillMount() {
     window.scrollTo(0, 0);
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="section center">
-          <div className="row">
-            <div className="col l8 m10 s12 offset-l2 offset-m1">
-              <div className="card">
-                <div className="card-content">
+      <div className='container'>
+        <div className='section center'>
+          <div className='row'>
+            <div className='col l8 m10 s12 offset-l2 offset-m1'>
+              <div className='card'>
+                <div className='card-content'>
                   <h2>Join Our Mailing List!</h2>
                   <p>Become a part of the movement that is reshaping the study of Tanach by signing up below!</p>
-                  <p>We <b>never</b> use your email for anything other than daily emails and event notifications. If you have any questions feel free to <Link to="/contact">contact us</Link> at your leisure.</p>
+                  <p>We <b>never</b> use your email for anything other than daily emails and event notifications. If you have any questions feel free to <Link to='/contact'>contact us</Link> at your leisure.</p>
                 </div>
               </div>
             </div>
-            <div className="col l8 m10 s12 offset-l2 offset-m1">
-            <div className="red-text error">{this.state.errorMsg}</div>
-              <div className="card">
-                <div className="card-content">
-                  <SignupForm error={(msg) => this.updateState('errorMsg', msg)}/>
+            <div className='col l8 m10 s12 offset-l2 offset-m1'>
+            <div className='red-text error'>{this.state.errorMsg}</div>
+              <div className='card'>
+                <div className='card-content'>
+                  <SignupForm error={(msg) => this.setState({ errorMsg: msg })}/>
                 </div>
               </div>
             </div>
