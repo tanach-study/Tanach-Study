d0e97763fe9dac9b461ce3ed1a956c2409facd4f
Joey Pinhas <jpinhas97@yahoo.com>
2018-04-27 17:10:25 -0400

    Changes text of history page, and adds video

--- src/components/About/History.jsx
+++ src/components/About/History.jsx
@@ -3,30 +3,14 @@ import React from 'react';
 function History (props) {
   return (
     <div>
-      <h3 className='left-align'>Our Humble Beginnings</h3>
-      <div className='row valign-wrapper hide-on-small-only'>
-        <div className='col l3 m4'>
-          <img src='https://cdn.tanachstudy.com/assets/images/beginning.jpg' alt='beginning start button' className='responsive-img'/>
-        </div>
-        <div className='col l9 m8'>
-          <p>It became apparent that our most important work to our nation had started to become neglected on some level. We all knew the Tanach was the backbone to our identity, but no one knew how to increase its study in a serious and sanctified manner.</p>
-          <p>One morning in the Sephardic Rabbinical College, Rabbi Albert Setton announced that he took a new position at Kol Israel and wanted to teach over Sefer Yeshaya.</p>
-          <p>Jesse quickly interjected, “What do you think of recording those perakim and using them to create an online platform to study all of Tanach?” With his affirmation, Jesse turned to Ovadia and said “We’ll get your dad to teach Yirmiyahu. Can you teach Yehezkel? And Josh, you’ll do Tere Asar?” Within 60 seconds there were commitments to teaching all of Nevi’im Aharonim, and with the hardest sefarim accounted for, Jesse knew that with a little effort he could help create a platform for a textual study of the entire Tanach. He set his mind to completing this mission, and ran from rabbi to rabbi and from educator to developer pitching the concept. Nearly everyone he approached identified with the idea and enthusiastically dedicated their time to make it a reality. With the help of a few dedicated young men and women a website was launched, texts were written and summarized, recordings were recorded, and the program was publicized. The program launched in October 2015 and grew to 1000 subscribers within a few weeks.</p>
-          <p>Today we look back in amazement how a few inconsequential words and a strong will, turned into a long overdue Tanach study platform for the world to utilize.</p>
-        </div>
+      <div className='center section'>
+        <iframe width='560' height='315' src='https://www.youtube.com/embed/RrTA85DLeZ4?rel=0' frameborder='1' allow='autoplay; encrypted-media' allowfullscreen></iframe>
       </div>
-
-      <div className='row hide-on-med-and-up'>
-        <div className='col s12'>
-          <img src='https://cdn.tanachstudy.com/assets/images/beginning.jpg' alt='beginning start button' className='responsive-img'/>
-        </div>
-        <div className='col s12'>
-          <p>It became apparent that our most important work to our nation had started to become neglected on some level. We all knew the Tanach was the backbone to our identity, but no one knew how to increase its study in a serious and sanctified manner.</p>
-          <p>One morning in the Sephardic Rabbinical College, Rabbi Albert Setton announced that he took a new position at Kol Israel and wanted to teach over Sefer Yeshaya.</p>
-          <p>Jesse quickly interjected, “What do you think of recording those perakim and using them to create an online platform to study all of Tanach?” With his affirmation, Jesse turned to Ovadia and said “We’ll get your dad to teach Yirmiyahu. Can you teach Yehezkel? And Josh, you’ll do Tere Asar?” Within 60 seconds there were commitments to teaching all of Nevi’im Aharonim, and with the hardest sefarim accounted for, Jesse knew that with a little effort he could help create a platform for a textual study of the entire Tanach. He set his mind to completing this mission, and ran from rabbi to rabbi and from educator to developer pitching the concept. Nearly everyone he approached identified with the idea and enthusiastically dedicated their time to make it a reality. With the help of a few dedicated young men and women a website was launched, texts were written and summarized, recordings were recorded, and the program was publicized. The program launched in October 2015 and grew to 1000 subscribers within a few weeks.</p>
-          <p>Today we look back in amazement how a few inconsequential words and a strong will, turned into a long overdue Tanach study platform for the world to utilize.</p>
-        </div>
-      </div>
+      <h3>Our Humble Beginnings</h3>
+      <p>It became apparent that our most important work to our nation had started to become neglected on some level. We all knew the Tanach was the backbone to our identity, but no one knew how to increase its study in a serious and sanctified manner.</p>
+      <p>One morning a side conversation transpired amongst a few committed young professionals at the Sephardic Rabbinical College, and within 20 minutes a concept to create a platform for a textual study of the entire Tanach began to emerge.</p>
+      <p>Rabbis, educators and developers were all pitched the idea. Nearly everyone approached identified with the notion and enthusiastically dedicated their time to make it a reality. With the help of a few dedicated young men and women, a website was launched, texts were summarized, classes were recorded, and the program was publicized. Tanach Study launched in October 2015 and grew to over 1,000 subscribers within a few weeks.</p>
+      <p>Today, we look back in amazement at how a few inconsequential words and a strong will were actualized into a long overdue platform for the study of Tanach that the world can utilize.</p>
     </div>
   );
 }
