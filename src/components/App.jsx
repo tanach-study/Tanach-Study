713afdebe3fc270da01e165598bc77961947eabc
Joey Pinhas <jpinhas97@yahoo.com>
2018-05-30 20:03:18 -0400

    Update parasha link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,7 @@ function App(props) {
           <Route exact path='/sefarim/:sefer' component={Sefarim} />
           <Route exact path='/parts' component={Parts} />
           <Route path='/about' component={About} />
-          <Redirect from='/parasha' to='/perakim/bemidbar/naso' />
+          <Redirect from='/parasha' to='/perakim/bemidbar/behaalotecha' />
           <Route path='/' component={HomePage} />
         </Switch>
       </div>
