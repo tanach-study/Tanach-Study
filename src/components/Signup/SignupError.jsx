f5a7ca2813986856f069e5913fa0b03a268190a9
Joey Pinhas <jpinhas97@yahoo.com>
2018-10-05 00:10:31 -0400

    Update the error message for errors during signup

--- src/components/Signup/SignupError.jsx
+++ src/components/Signup/SignupError.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignupError = props => {
   return (
     <div>
-      YOU ARE A FAILURE
+      <h2 className='card-title'>Hmm Something's Wrong...</h2>
+      <p>Looks like there was an error signing you up to our list - we apologize! Here's why:</p>
+      <br/>
+      <p>{props.message || "Unknown error"}</p>
+      <br/>
+      <p>How about you <span className='blue-text clickable' onClick={props.resetForm}>try again?</span> If the problem persists, feel free to reach out to us <Link to='/contact'>to let us know.</Link></p>
     </div>
   )
 }
