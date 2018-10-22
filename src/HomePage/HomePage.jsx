b0626c22da98d59934613eb69e6cc5a24532c3a4
Joey Pinhas <jpinhas97@yahoo.com>
2018-10-22 01:18:54 -0400

    Remove signup form from home page

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Slider from './Slider/Slider.jsx';
 import Testimonials from './Testimonials/Testimonials.jsx';
 import testimonials from './Testimonials/testimonials.json';
-import HomeSignupForm from './HomeSignupForm/HomeSignupForm.jsx';
 
 import styles from './HomePage.css';
 import icons from '../icons.css';
@@ -155,19 +154,6 @@ class HomePage extends Component {
             <img src='https://cdn.tanachstudy.com/assets/images/analytics.png' alt='' className='responsive-img'/>
           </div>
         </div>
-
-        <div className='section'>
-          <div className='container'>
-            <div className='card'>
-              <div className='card-content center'>
-                <h2>Sign Up Today!</h2>
-                <div className='red-text error'>{this.state.errorMsg}</div>
-                <HomeSignupForm error={(msg) => this.setState({ errorMsg: msg })} />
-              </div>
-            </div>
-          </div>
-        </div>
-
       </div>
     );
   }
