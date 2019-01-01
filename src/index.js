66d7c217dada0d08801d58eb4aa4832fa99c0e71
Joey Pinhas <jpinhas97@yahoo.com>
2018-12-31 19:07:00 -0500

    Clean up code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React    from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import ReactGA from 'react-ga';
 import 'whatwg-fetch';
 
-import ReactGA from 'react-ga';
+import App from './App.jsx';
+
 ReactGA.initialize('UA-53814701-1');
 
 // https://github.com/ReactTraining/react-router/issues/4278#issuecomment-299692502
@@ -15,10 +16,12 @@ const withTracker = (WrappedComponent) => {
   };
 
   const HOC = (props) => {
-    const page = props.location.pathname + props.location.search;
+    const { location } = props;
+    const page = location.pathname + location.search;
     trackPage(page);
 
     return (
+      /* eslint "react/jsx-filename-extension": [0] */
       <WrappedComponent {...props} />
     );
   };
@@ -30,5 +33,5 @@ ReactDOM.render(
   <BrowserRouter>
     <Route component={withTracker(App)} />
   </BrowserRouter>,
-  document.querySelector('#root-container')
+  document.querySelector('#root-container'),
 );
