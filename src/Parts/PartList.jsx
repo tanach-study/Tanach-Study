e750142e95f13e934b519f16492edf7f711f582a
Joey Pinhas <jpinhas97@yahoo.com>
2018-12-31 20:05:23 -0500

    Clean up code

--- src/Parts/PartList.jsx
+++ src/Parts/PartList.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import PartLink from './PartLink.jsx';
 
 function PartList(props) {
-  const mapped = props.parts.map((book, i, books) => <PartLink key={`part-link-${book.name}`} book={book} i={i} books={books} />);
+  const { parts, title } = props;
+  const mapped = parts.map((book, i, books) => (
+    <PartLink key={`part-link-${book.name}`} book={book} i={i} books={books} />
+  ));
   return (
     <div className='row center'>
       <div className='center center-align light'>
-        <h5>{props.title}</h5>
+        <h5>{title}</h5>
         {mapped}
       </div>
     </div>
