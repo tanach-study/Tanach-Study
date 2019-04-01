09084634c0040194f3af8c6f1a6bbe2cce1b58e9
Joey Pinhas <jpinhas97@gmail.com>
2019-04-01 12:04:50 -0400

    Create basic structure of TS homepage

--- src/TanachStudy/TanachStudyHomePage/TanachStudyHomePage.jsx
+++ src/TanachStudy/TanachStudyHomePage/TanachStudyHomePage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function TanachStudyHomePage(props) {
   return (
-    <h1>TS Home</h1>
+    <main className='container'>
+      <section className='section center'>
+        <p>Currently, Tanach Study is studying Yehoshua Perek 1.</p>
+        <Link to='/tanach-study/1'>Click here to go to today&#39;s part</Link>
+      </section>
+      <section className='section center'>
+        <Link to='/tanach-study/parts'>
+          <button className='btn tsblue'>Study a Perek</button>
+        </Link>
+      </section>
+    </main>
   );
 }
 
