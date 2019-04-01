755874f36700537d050a1391af0ded6a92a6367d
Joey Pinhas <jpinhas97@gmail.com>
2019-04-01 12:13:32 -0400

    Update TS homepage to be stateful, and lay out the page better

--- src/TanachStudy/TanachStudyHomePage/TanachStudyHomePage.jsx
+++ src/TanachStudy/TanachStudyHomePage/TanachStudyHomePage.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-function TanachStudyHomePage(props) {
-  return (
-    <main className='container'>
-      <section className='section center'>
-        <p>Currently, Tanach Study is studying Yehoshua Perek 1.</p>
-        <Link to='/tanach-study/1'>Click here to go to today&#39;s part</Link>
-      </section>
-      <section className='section center'>
-        <Link to='/tanach-study/parts'>
-          <button className='btn tsblue'>Study a Perek</button>
-        </Link>
-      </section>
-    </main>
-  );
+class TanachStudyHomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPerek: {
+        division: '',
+        section: 'Yehoshua',
+        unit: '2',
+        part: '',
+        series: '',
+      },
+    };
+  }
+
+  componentWillMount() {
+    // TODO: implement
+    // get data from API
+    // create link
+    // save to state
+  }
+
+  render() {
+    const { currentPerek } = this.state;
+    const { section, unit } = currentPerek;
+    return (
+      <main className='container'>
+        <section className='section center'>
+          <h3>Welcome to Tanach Study!</h3>
+          <p>Currently, Tanach Study is studying Sefer {section} Perek {unit}.</p>
+          <Link to='/tanach-study/1'>Click here to go to today&#39;s part</Link>
+        </section>
+        <section className='section center'>
+          <Link to='/tanach-study/parts'>
+            <button className='btn tsblue'>Study a Perek</button>
+          </Link>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default TanachStudyHomePage;
