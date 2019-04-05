6f1fba84e697da5c10c82a62293ed948b667970d
Joey Pinhas <jpinhas97@gmail.com>
2019-04-05 16:36:12 -0400

    Add real data to app context

--- src/app-context.js
+++ src/app-context.js
@@ -2,28 +2,20 @@ import React from 'react';
 
 export const programs = {
   tanachstudy: {
-    mainColor: '#111111',
-    secondaryColor: '#222222',
-    tertiaryColor: '#333333',
-    complementaryColor: '#444444',
-    logo: '/assets/images/logos/tanachstudy.png',
+    mainColor: '#009FC1',
+    secondaryColor: '#E5F3F7',
+    tertiaryColor: '#039BE5',
+    complementaryColor: '#FFB023',
+    logo: 'https://cdn.tanachstudy.com/assets/images/logo.png',
     linkPrefix: 'tanach-study',
   },
   mishnastudy: {
-    mainColor: '#111111',
-    secondaryColor: '#222222',
-    tertiaryColor: '#333333',
-    complementaryColor: '#444444',
-    logo: '/assets/images/logos/mishnastudy.png',
+    mainColor: '#950010',
+    secondaryColor: '#F2E1E2',
+    tertiaryColor: '#D33447',
+    complementaryColor: '#2AAD5F',
+    logo: 'https://cdn.tanachstudy.com/assets/images/mishna-study-logo.png',
     linkPrefix: 'mishna-study',
-  },
-  haftarastudy: {
-    mainColor: '#111111',
-    secondaryColor: '#222222',
-    tertiaryColor: '#333333',
-    complementaryColor: '#444444',
-    logo: '/assets/images/logos/mishnastudy.png',
-    linkPrefix: 'haftara-study',
   },
 };
 
