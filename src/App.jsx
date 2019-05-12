7d602c64472f18ebf8d01b012de2e131c451ee66
Ari Mendelow <ari@mendelow.me>
2019-05-12 16:43:48 -0500

    Move getParasha function

--- src/App.jsx
+++ src/App.jsx
@@ -20,59 +20,6 @@ import MishnaStudy from './MishnaStudy/MishnaStudy.jsx';
 
 import { programs, ProgramContext } from './app-context.js';
 
-function getParasha() {
-  const today = new Date();
-  const month = today.getMonth(); // 0-11
-  const date = today.getDate(); // 1-31
-
-  const OCTOBER = 9;
-  const NOVEMBER = 10;
-  const DECEMBER = 11;
-
-  let parasha = '';
-
-  if (month === OCTOBER) {
-    if (date < 6) { // bereshit is september 30 - october 6, inclusive
-      parasha = 'bereshit';
-    } else if (date < 13) { // noah is october 7-13, inclusive
-      parasha = 'noah';
-    } else if (date < 20) { // lech lecha is october 14-20, inclusive
-      parasha = 'lech-lecha';
-    } else if (date < 28) { // vayera is october 21-27, inclusive
-      parasha = 'vayera';
-    } else if (date < 32) { // haye sarah is october 28 - november 3, inclusive
-      parasha = 'haye-sarah';
-    }
-  } else if (month === NOVEMBER) {
-    if (date < 3) { // haye sarah is october 28 - november 3, inclusive
-      parasha = 'haye-sarah';
-    } else if (date < 10) { // toledot is november 4-10, inclusive
-      parasha = 'toledot';
-    } else if (date < 17) { // vayetze is november 11-17, inclusive
-      parasha = 'vayetze';
-    } else if (date < 24) { // vayishlah is november 18-24, inclusive
-      parasha = 'vayishlah';
-    } else if (date < 31) { // vayeshev is november 25 - december 1, inclusive
-      parasha = 'vayeshev';
-    }
-  } else if (month === DECEMBER) {
-    if (date < 1) { // vayeshev is november 25 - december 1, inclusive
-      parasha = 'vayeshev';
-    } else if (date < 8) { // toledot is december 2-8, inclusive
-      parasha = 'miketz';
-    } else if (date < 15) { // vayigash is december 9-15, inclusive
-      parasha = 'vayigash';
-    } else if (date < 22) { // vayhi is december 16-22, inclusive
-      parasha = 'vayhi';
-    } else if (date < 29) { // shemot is december 23-29, inclusive
-      parasha = 'shemot';
-    } else if (date < 32) { // vaera is december 30 - january 5, inclusive
-      parasha = 'vaera';
-    }
-  }
-  return parasha;
-}
-
 class App extends Component {
   constructor(props) {
     super(props);
