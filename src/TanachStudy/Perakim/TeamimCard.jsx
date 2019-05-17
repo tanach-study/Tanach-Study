a53edf78734810b48d179b279dce9ddb72b7c9be
Joey Pinhas <jpinhas97@gmail.com>
2019-05-17 11:07:36 -0400

    Update to create tabs for the different parts of a perek in Nach

--- src/TanachStudy/Perakim/TeamimCard.jsx
+++ src/TanachStudy/Perakim/TeamimCard.jsx
@@ -23,6 +23,7 @@ class ReaderCard extends Component {
     if (teamim) {
       if (Array.isArray(teamim)) {
         if (teamim.length > 1) {
+          console.log(teamim)
           const teamimTabs = teamim.map((item, i) => (
             <li className='tab' key={item.url}>
               <div
