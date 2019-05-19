6a81e13e4833e434dd95f465e6f43b95763a606c
Ari Mendelow <ari@mendelow.me>
2019-05-19 18:54:41 -0500

    Refactor created by and social sections from footer to be functional components

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,9 +3,78 @@ import { Link } from 'react-router-dom';
 
 import styles from './Footer.css';
 
+function CreatedBy() {
+  return (
+    <React.Fragment>
+      <span className='white-text'>Created by </span>
+      <a
+        className='black-text text-lighten-3'
+        href='https://www.linkedin.com/in/joeypinhas/'
+        target='blank'
+      >
+        Joey Pinhas
+      </a>
+    </React.Fragment>
+  );
+}
+
+function SocialLinks() {
+  return (
+    <React.Fragment>
+      <Link to='/signup' className={styles['social-icons']}>
+        <img
+          src='https://cdn.tanachstudy.com/assets/icons/003-envelope.svg'
+          alt='Email Icon'
+        />
+      </Link>
+      <a
+        href='https://www.facebook.com/tanachstudy'
+        className={styles['social-icons']}
+        target='blank'
+      >
+        <img
+          src='https://cdn.tanachstudy.com/assets/icons/037-facebook-logo.svg'
+          alt='FaceBook Icon'
+        />
+      </a>
+      <a
+        href='https://itunes.apple.com/us/podcast/tanach-study/id930200652'
+        className={styles['social-icons']}
+        target='blank'
+      >
+        <img
+          src='https://cdn.tanachstudy.com/assets/icons/007-podcast-big-logo.svg'
+          alt='Podcasts Icon'
+        />
+      </a>
+      <a
+        href='https://www.instagram.com/tanach_study'
+        className={styles['social-icons']}
+        target='blank'
+      >
+        <img
+          src='https://cdn.tanachstudy.com/assets/icons/020-instagram-1.svg'
+          alt='Instagram Icon'
+        />
+      </a>
+      <a
+        href='https://www.youtube.com/channel/UC0b52aDc7f8VTtxT-Ktfb6Q/feed'
+        className={styles['social-icons']}
+        target='blank'
+      >
+        <img
+          src='https://cdn.tanachstudy.com/assets/icons/036-youtube-logo.svg'
+          alt='YouTube Icon'
+        />
+      </a>
+    </React.Fragment>
+  );
+}
+
 function Footer(props) {
   const { program } = props || {};
   const { linkPrefix } = program || '';
+  const windowWidth = window.innerWidth || '';
   let bgColorClass = 'tsblue';
   switch (linkPrefix) {
     case 'mishna-study':
@@ -26,62 +95,10 @@ function Footer(props) {
       <div className={`container ${styles['footer-container']}`}>
         <div className={`valign-wrapper row ${styles['footer-wrapper']}`}>
           <div className='col l6 m6 s6 left-align valign'>
-            <span className='white-text'>Created by </span>
-            <a
-              className='black-text text-lighten-3'
-              href='https://www.linkedin.com/in/joeypinhas/'
-              target='blank'
-            >
-              Joey Pinhas
-            </a>
+            <CreatedBy />
           </div>
           <div className='col l6 m6 s6 right-align valign'>
-            <Link to='/signup' className={styles['social-icons']}>
-              <img
-                src='https://cdn.tanachstudy.com/assets/icons/003-envelope.svg'
-                alt='Email Icon'
-              />
-            </Link>
-            <a
-              href='https://www.facebook.com/tanachstudy'
-              className={styles['social-icons']}
-              target='blank'
-            >
-              <img
-                src='https://cdn.tanachstudy.com/assets/icons/037-facebook-logo.svg'
-                alt='FaceBook Icon'
-              />
-            </a>
-            <a
-              href='https://itunes.apple.com/us/podcast/tanach-study/id930200652'
-              className={styles['social-icons']}
-              target='blank'
-            >
-              <img
-                src='https://cdn.tanachstudy.com/assets/icons/007-podcast-big-logo.svg'
-                alt='Podcasts Icon'
-              />
-            </a>
-            <a
-              href='https://www.instagram.com/tanach_study'
-              className={styles['social-icons']}
-              target='blank'
-            >
-              <img
-                src='https://cdn.tanachstudy.com/assets/icons/020-instagram-1.svg'
-                alt='Instagram Icon'
-              />
-            </a>
-            <a
-              href='https://www.youtube.com/channel/UC0b52aDc7f8VTtxT-Ktfb6Q/feed'
-              className={styles['social-icons']}
-              target='blank'
-            >
-              <img
-                src='https://cdn.tanachstudy.com/assets/icons/036-youtube-logo.svg'
-                alt='YouTube Icon'
-              />
-            </a>
+            <SocialLinks />
           </div>
         </div>
       </div>
