19dc308dcdc14460c854167a9f2c7f1e29b27a43
Perry Raskin <perryraskin@gmail.com>
2019-05-20 22:02:44 -0400

    Fixed linter errors

--- src/ProgramNavigation/ProgramNavigation.jsx
+++ src/ProgramNavigation/ProgramNavigation.jsx
@@ -1,48 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './ProgramNavigation.css';
 
-class ProgramNavigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <nav className={`${styles['program-navigation']}`}>
-        <ul className='valign-wrapper'>
-          <li className={styles['nav-list-item']}>
-            <Link to='/tanach-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/logo.png' alt='Tanach Study Logo' />
-            </Link>
-          </li>
-          <li className={styles['nav-list-item']}>
-            <Link to='/parasha-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/parasha-study-logo.png' alt='Parasha Study Logo' />
-            </Link>
-          </li>
-          <li className={styles['nav-list-item']}>
-            <Link to='/mishna-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/mishna-study-logo.png' alt='Mishna Study Logo' />
-            </Link>
-          </li>
-          <li className={styles['nav-list-item']}>
-            <Link to='/haftara-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/haftara-study-logo.png' alt='Haftara Study Logo' />
-            </Link>
-          </li>
-          <li className={styles['nav-list-item']}>
-            <Link to='/moadim-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/moadim-study-logo.png' alt='Moadim Study Logo' />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+function ProgramNavigation(props) {
+  return (
+    <nav className={`${styles['program-navigation']}`}>
+      <ul className='valign-wrapper'>
+        <li className={styles['nav-list-item']}>
+          <Link to='/tanach-study' className={styles['nav-link']}>
+            <img
+              src='https://cdn.tanachstudy.com/assets/images/logo.png'
+              alt='Tanach Study Logo'
+            />
+          </Link>
+        </li>
+        <li className={styles['nav-list-item']}>
+          <Link to='/parasha-study' className={styles['nav-link']}>
+            <img
+              src='https://cdn.tanachstudy.com/assets/images/parasha-study-logo.png'
+              alt='Parasha Study Logo'
+            />
+          </Link>
+        </li>
+        <li className={styles['nav-list-item']}>
+          <Link to='/mishna-study' className={styles['nav-link']}>
+            <img
+              src='https://cdn.tanachstudy.com/assets/images/mishna-study-logo.png'
+              alt='Mishna Study Logo'
+            />
+          </Link>
+        </li>
+        <li className={styles['nav-list-item']}>
+          <Link to='/haftara-study' className={styles['nav-link']}>
+            <img
+              src='https://cdn.tanachstudy.com/assets/images/haftara-study-logo.png'
+              alt='Haftara Study Logo'
+            />
+          </Link>
+        </li>
+        <li className={styles['nav-list-item']}>
+          <Link to='/moadim-study' className={styles['nav-link']}>
+            <img
+              src='https://cdn.tanachstudy.com/assets/images/moadim-study-logo.png'
+              alt='Moadim Study Logo'
+            />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default ProgramNavigation;
