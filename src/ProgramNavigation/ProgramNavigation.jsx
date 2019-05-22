ec15ec67489c7bc960d9efc4e6012b772f1c96fc
Ari Mendelow <ari@mendelow.me>
2019-05-21 23:25:47 -0500

    Add tabbing styling on program nav bar

--- src/ProgramNavigation/ProgramNavigation.jsx
+++ src/ProgramNavigation/ProgramNavigation.jsx
@@ -11,30 +11,33 @@ class ProgramNavigation extends Component {
   }
 
   render() {
+    const { location } = this.props;
+    const { pathname } = location;
+    console.log(pathname);
     return (
       <nav className={`${styles['program-navigation']}`}>
         <ul className='valign-wrapper'>
-          <li className={styles['nav-list-item']}>
+          <li className={pathname.includes('/tanach-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
             <Link to='/tanach-study' className={styles['nav-link']}>
               <img src='https://cdn.tanachstudy.com/assets/images/logo.png' alt='Tanach Study Logo' />
             </Link>
           </li>
-          <li className={styles['nav-list-item']}>
+          <li className={pathname.includes('/mishna-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
             <Link to='/mishna-study' className={styles['nav-link']}>
               <img src='https://cdn.tanachstudy.com/assets/images/mishna-study-logo.png' alt='Mishna Study Logo' />
             </Link>
           </li>
-          <li className={styles['nav-list-item']}>
+          <li className={pathname.includes('/haftara-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
             <Link to='/haftara-study' className={styles['nav-link']}>
               <img src='https://cdn.tanachstudy.com/assets/images/haftara-study-logo.png' alt='Haftara Study Logo' />
             </Link>
           </li>
-          <li className={styles['nav-list-item']}>
+          <li className={pathname.includes('/parasha-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
             <Link to='/parasha-study' className={styles['nav-link']}>
               <img src='https://cdn.tanachstudy.com/assets/images/parasha-study-logo.png' alt='Parasha Study Logo' />
             </Link>
           </li>
-          <li className={styles['nav-list-item']}>
+          <li className={pathname.includes('/moadim-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
             <Link to='/moadim-study' className={styles['nav-link']}>
               <img src='https://cdn.tanachstudy.com/assets/images/moadim-study-logo.png' alt='Moadim Study Logo' />
             </Link>
