dfbea1fd3ed8e4660f463837ee203044921e2120
Ari Mendelow <ari@mendelow.me>
2019-05-21 23:35:16 -0500

    Refactor to be more dynamic

--- src/ProgramNavigation/ProgramNavigation.jsx
+++ src/ProgramNavigation/ProgramNavigation.jsx
@@ -13,36 +13,53 @@ class ProgramNavigation extends Component {
   render() {
     const { location } = this.props;
     const { pathname } = location;
-    console.log(pathname);
     const activateTS = !(pathname.includes('/mishna-study') || pathname.includes('/haftara-study') || pathname.includes('/parasha-study') || pathname.includes('/moadim-study'));
+
+    const links = [
+      {
+        to: '/tanach-study',
+        img: 'https://cdn.tanachstudy.com/assets/images/logo.png',
+        alt: 'Tanach Study Logo',
+        activate: !(pathname.includes('/mishna-study') || pathname.includes('/haftara-study') || pathname.includes('/parasha-study') || pathname.includes('/moadim-study')),
+      },
+      {
+        to: '/mishna-study',
+        img: 'https://cdn.tanachstudy.com/assets/images/mishna-study-logo.png',
+        alt: 'Mishna Study Logo',
+        activate: pathname.includes('/mishna-study'),
+      },
+      {
+        to: '/haftara-study',
+        img: 'https://cdn.tanachstudy.com/assets/images/haftara-study-logo.png',
+        alt: 'Haftara Study Logo',
+        activate: pathname.includes('/haftara-study'),
+      },
+      {
+        to: '/parasha-study',
+        img: 'https://cdn.tanachstudy.com/assets/images/parasha-study-logo.png',
+        alt: 'Parasha Study Logo',
+        activate: pathname.includes('/parasha-study'),
+      },
+      {
+        to: '/moadim-study',
+        img: 'https://cdn.tanachstudy.com/assets/images/moadim-study-logo.png',
+        alt: 'Moadim Study Logo',
+        activate: pathname.includes('/moadim-study'),
+      },
+    ]
+
+    const mappedLinks = links.map(l => (
+      <li className={l.activate ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
+        <Link to={l.to} className={styles['nav-link']}>
+          <img src={l.img} alt={l.alt} />
+        </Link>
+      </li>
+    ));
+    
     return (
       <nav className={`${styles['program-navigation']}`}>
         <ul className='valign-wrapper'>
-          <li className={activateTS ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
-            <Link to='/tanach-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/logo.png' alt='Tanach Study Logo' />
-            </Link>
-          </li>
-          <li className={pathname.includes('/mishna-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
-            <Link to='/mishna-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/mishna-study-logo.png' alt='Mishna Study Logo' />
-            </Link>
-          </li>
-          <li className={pathname.includes('/haftara-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
-            <Link to='/haftara-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/haftara-study-logo.png' alt='Haftara Study Logo' />
-            </Link>
-          </li>
-          <li className={pathname.includes('/parasha-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
-            <Link to='/parasha-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/parasha-study-logo.png' alt='Parasha Study Logo' />
-            </Link>
-          </li>
-          <li className={pathname.includes('/moadim-study') ? styles['nav-list-item-selected'] : styles['nav-list-item']}>
-            <Link to='/moadim-study' className={styles['nav-link']}>
-              <img src='https://cdn.tanachstudy.com/assets/images/moadim-study-logo.png' alt='Moadim Study Logo' />
-            </Link>
-          </li>
+          {mappedLinks}
         </ul>
       </nav>
     );
