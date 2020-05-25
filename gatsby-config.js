8b5fb41528a8547424f6684491b47bf844caadd4
Joey Pinhas <jpinhas97@gmail.com>
2020-05-25 00:25:44 -0400

    Remove images

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
