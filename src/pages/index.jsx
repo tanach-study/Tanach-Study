791ddbd27d44b2610f5e4273b60f8043ec110441
Joey Pinhas <jpinhas97@gmail.com>
2020-06-17 23:11:10 -0400

    Show basic homepage on dev

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import Layout from '../layouts/main.jsx';
+
 const IndexPage = () => {
-  if (typeof window !== 'undefined') window.location.replace('https://tanachstudy.com');
+  if (typeof window !== 'undefined') {
+    if (window.location.hostname !== 'localhost') {
+      return window.location.replace('https://tanachstudy.com');
+    }
+  }
   return (
-    <></>
+    <Layout>
+      <p>Welcome home!</p>
+    </Layout>
   );
 };
 
