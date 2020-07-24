9d254316a005e6ccceae63b70f3323597f5b3591
Joey Pinhas <jpinhas97@gmail.com>
2020-07-23 22:10:57 -0400

    Refactor extra dependency

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import classNames from 'classnames';
-
 import styles from './Navigation.module.css';
 
 class Navigation extends Component {
@@ -21,20 +19,12 @@ class Navigation extends Component {
 
   render() {
     const { isMobileActive } = this.state;
-    const mobileNavClass = classNames({
-      [styles.tsnav]: true,
-      [styles.active]: isMobileActive,
-    });
-    const navListClass = classNames({
-      [styles['nav-list']]: true,
-      [styles.active]: isMobileActive,
-    });
     return (
-      <nav className={`white ${mobileNavClass}`}>
+      <nav className={`white ${styles.tsnav} ${isMobileActive ? styles.active : ''}`}>
         <a className={styles['nav-image-link']} href='https://tanachstudy.com'>
           <img className={styles['nav-image']} src='https://cdn.tanachstudy.com/assets/images/logo.png' alt='Tanach Study' />
         </a>
-        <ul className={navListClass}>
+        <ul className={`${styles['nav-list']} ${isMobileActive ? styles.active : ''}`}>
           <li><a href='https://tanachstudy.com'>Home</a></li>
           <li><a href='https://tanachstudy.com/about'>About</a></li>
           <li><a href='https://tanachstudy.com/signup'>Sign Up</a></li>
