fb3725178ff95c0f4da3d2c058a7e672c156cf2b
Joey Pinhas <jpinhas97@gmail.com>
2020-07-23 22:02:08 -0400

    Remove extraneous dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
