1f8952333d838a8c19856ff5cb7df702c5258c2c
Joey Pinhas <jpinhas97@gmail.com>
2020-10-18 12:23:51 -0400

    Add support for multiple series

--- src/templates/ParashaStudyPlus/Sefarim/SeferList/SeferItem.jsx
+++ src/templates/ParashaStudyPlus/Sefarim/SeferList/SeferItem.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 import { ProgramContext } from '../../../../app-context.js';
 
 const SeferItem = (props) => {
-  const { title, index, click, selected } = props;
+  const { title, index, click, selected, series } = props;
   return (
     <ProgramContext.Consumer>
       {(program) => {
         const { backgroundClass: bg } = program;
         /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+        if (series && series !== null && series !== '') {
+          return (
+            <li
+              className={`collection-item clickable ${selected === index ? `active ${bg}` : ''}`}
+              onClick={() => click(index)}
+            >
+              <span>Parashat {title}: {series} Round</span>
+            </li>
+          );
+        }
         return (
           <li
             className={`collection-item clickable ${selected === index ? `active ${bg}` : ''}`}
